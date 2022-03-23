173229a35b100707bae7379a435742081e918359
Lalitha Merugu <lalitha.chot@gmail.com>
2022-03-23 11:48:31 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ButtonsComponent } from './components/buttons/buttons.component';
+import { CardsComponent } from './components/cards/cards.component';
+//import { ButtonsComponent } from './components/buttons/buttons.component';
 
-const routes: Routes = [];
+
+const appRoutes: Routes = [
+    { path:' ',redirectTo:'cards',pathMatch:'full' },
+  //{ path: 'cards', component: ButtonsComponent },
+ { path: 'buttons', component: ButtonsComponent },
+ { path: 'cards', component: CardsComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { ButtonsComponent } from './components/buttons/buttons.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent,
+    ButtonsComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
